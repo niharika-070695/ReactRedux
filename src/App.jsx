fc1f97866bff9759b1b902b093dfef020ccadb74
niharika0706 <harika5991@gmail.com>
2024-07-19 23:37:25 -0400

    Commit till session-21

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Ref from "./components/ref/Ref";
+import Timer from "./components/timer/Timer";
 
 function App() {
   return (
     <div className="app">
       <Ref />
+      <Timer />
     </div>
   );
 }
--- src/components/ref/Ref.jsx
+++ src/components/ref/Ref.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./Ref.css";
 function Ref() {
   let [state, setState] = useState(null);
   let [data, setData] = useState(null);
+  let info = useRef("hello");
   let value;
   const updateState = () => {
     setState(100);
@@ -14,10 +15,15 @@ function Ref() {
   const updateData = () => {
     setData(1000);
   };
+
+  const updateInfo = () => {
+    info.current = "Welcome to React";
+  };
   const displayValue = () => {
     console.log("State", state);
     console.log("Data", data);
     console.log("Variable", value);
+    console.log("UseRef-info", info.current);
   };
   return (
     <div className="refContainer">
@@ -25,6 +31,7 @@ function Ref() {
       <button onClick={updateState}>Update State</button>
       <button onClick={updateData}>Update Data</button>
       <button onClick={updateValue}>Update Value</button>
+      <button onClick={updateInfo}>Update Info</button>
       <button onClick={displayValue}>Display</button>
     </div>
   );
